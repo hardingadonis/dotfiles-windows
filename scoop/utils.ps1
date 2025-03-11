850af996e4418f319f5b8f92f2575d41266dc6c4
Minh Vương <hardingadonis@gmail.com>
2025-03-11 08:05:48 +0700

    update: add .editorconfig for consistent coding styles and create utility functions for package installation

--- scoop/utils.ps1
+++ scoop/utils.ps1
@@ -11,22 +11,15 @@
 #  ⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿⣿
 #  ⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿
 
-if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Output "scoop is not installed"
-    Write-Output "Installing scoop..."
+function Install-Package {
+	param (
+		[string]$packageName
+	)
 
-    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-
-    Write-Output "scoop installed"
-} else {
-    Write-Output "scoop is already installed"
+	if (-not (scoop list | Select-String -Pattern $packageName)) {
+		Write-Output "$packageName is not installed. Installing..."
+		scoop install $packageName
+	} else {
+		Write-Output "$packageName is already installed."
+	}
 }
-
-# Load the function from utils.ps1
-. "$PSScriptRoot/utils.ps1"
-
-# Install git for scoop
-Install-Package -packageName "git"
-
-# Add scoop buckets
-scoop bucket add extras
--- softwares/install-common-package.ps1
+++ softwares/install-common-package.ps1
@@ -11,22 +11,8 @@
 #  ⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿⣿
 #  ⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿
 
-if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Output "scoop is not installed"
-    Write-Output "Installing scoop..."
-
-    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-
-    Write-Output "scoop installed"
-} else {
-    Write-Output "scoop is already installed"
-}
-
 # Load the function from utils.ps1
-. "$PSScriptRoot/utils.ps1"
+. "$PSScriptRoot/../scoop/utils.ps1"
 
-# Install git for scoop
-Install-Package -packageName "git"
-
-# Add scoop buckets
-scoop bucket add extras
+# Install common packages
+Install-Package -packageName "extras/vcredist"
